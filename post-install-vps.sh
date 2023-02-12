64de5a9f482954e0597b98cc01b1eeedcd2ada3e
Gregory <gregory.leblond@akarah.com>
2023-02-12 18:48:17 +0100

    feat: post install packages script

---
+++ post-install-vps.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Usage: Post Install script for VPS
+# Author: Greg
+# -------------------------------------------------
+ 
+# Update the system
+sudo apt udpate -y
+sudo apt upgrade -y
+
+# Install nginx, php, and nodejs
+sudo apt install -y nginx php8.1-fpm php-mysql php-cli php-curl php-mbstring npm nodejs
+
+# Setup nginx
+sudo systemctl enable nginx
+
+# Setup nodejs
+sudo npm install -g pm2 n
+sudo n lts
